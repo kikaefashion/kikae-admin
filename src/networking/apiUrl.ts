45d5370e05aad508867e864b1b9ba54f15885aa6
Boogie2k <oghosabenjamin>
2025-10-26 15:25:30 +0100

    commit

--- src/networking/apiUrl.ts
+++ src/networking/apiUrl.ts
@@ -1,2 +1,2 @@
 export const baseUrl = process.env.NEXT_PUBLIC_BASE_URL;
-export const mediaUrlPrefix = process.env.NEXT_PUBLIC_MEDIA_URL_PREFIX;
+export const mediaUrlPrefix = process.env.NEXT_PUBLIC_MEDIA_URL_PREFIX || "";
