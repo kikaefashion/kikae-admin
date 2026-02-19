2d8b5e924fc9b7dd9f4e6b3e0c9f7cdf47f3c471
boogie2k <oghosabenjamin@gmail.com>
2026-02-19 01:57:02 +0100

    commit

--- src/app/admin_auth/admin_login/page.tsx
+++ src/app/admin_auth/admin_login/page.tsx
@@ -5,7 +5,7 @@ import { useRouter } from "next/navigation";
 import { useState } from "react";
 import { useForm } from "react-hook-form";
 
-import Loader from "@/components/Loader";
+//import Loader from "@/components/Loader";
 import { toast } from "react-toastify";
 
 type FormData = {
@@ -83,7 +83,7 @@ const Login = () => {
           disabled={isLoading}
           className="bg-kikaeBlue w-36 rounded-4xl p-2.5 mx-auto mt-6 justify-center"
         >
-          {isLoading ? <Loader /> : "  Login"}
+          {"Login"}
         </button>
       </form>
       <div className="flex flex-row justify-between items-center">
--- src/app/admin_auth/forgot-password/page.tsx
+++ src/app/admin_auth/forgot-password/page.tsx
@@ -7,7 +7,7 @@ import Link from "next/link";
 import { useState } from "react";
 import { useForm } from "react-hook-form";
 
-import Loader from "@/components/Loader";
+//import Loader from "@/components/Loader";
 import { toast } from "react-toastify";
 
 import Cookies from "universal-cookie";
@@ -88,7 +88,7 @@ const Login = () => {
           disabled={isLoading}
           className="bg-kikaeBlue w-36 rounded-4xl p-2.5 mx-auto mt-6 text-white justify-center"
         >
-          {isLoading ? <Loader /> : "Proceed"}
+          {"Proceed"}
         </button>
       </form>
       <Link href={"/"} className="text-kikaeBlue text-right">
